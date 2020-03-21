a16f01acf8f2d2f6c4763c82a5abf0842782b255
Glitch (alj-devops-santa-app-1) <none>
2020-03-22 02:23:46 +0900

    added checks

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = new express.Router();
+const axios = require('axios');
 
 const util = require('../utils')
 
-const {isEmpty} = util
+const {isEmpty, isRegisteredUser, isUserUnderAge} = util
 
-router.post('/', (request, response,) => {
+router.post('/', async (request, response,) => {
     const {body: {userid, wish}} = request
+
+    // simple form validation
     if(isEmpty(userid) || isEmpty(wish)) {
         request.flash('error', 'userid or wish cannot be empty')
         response.redirect('/error')
     }
 
+    const {data: usersData} = await axios.get('https://raw.githubusercontent.com/alj-devops/santa-data/master/users.json');
+    // check if user is registered
+    if(!isRegisteredUser(usersData, userid)) {
+        request.flash('error', 'user not registered')
+        response.redirect('/error')
+    }
+
+    const {data: usersProfile} = await axios.get('https://raw.githubusercontent.com/alj-devops/santa-data/master/userProfiles.json');
+    // check if user is under 10
+    if(!isUserUnderAge(usersData, usersProfile, userid)) {
+        request.flash('error', 'user should be less than 10 years old')
+        response.redirect('/error')
+    }
     response.redirect('/success')
 });
 
--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,59 @@
+
 var util = (function() {
     'use strict';
+
+    function getUser(usersData, requestedUser) {
+        let obj = usersData.find(o => o.username === requestedUser)
+        return obj
+    }
+
+    function getUserProfile(usersProfle, useruuid) {
+        let obj = usersProfle.find(o => o.userUid === useruuid)
+        return obj
+    }
+
+    function getAge(dateString) {
+        dateString = dateString.substr(0, 4)+"-"+dateString.substr(8, 2)+"-"+dateString.substr(5, 2);
+        console.log(dateString)
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
 
     function isEmpty(str) {
         return (!str || /^\s*$/.test(str));
     }
-  
+
+    function isRegisteredUser(usersData, requestedUser) {
+        const user = getUser(usersData, requestedUser)
+        if(!user) {
+            return false
+        }
+
+        return true
+    }
+
+    function isUserUnderAge(usersData, usersProfile, userid) {
+        const user = getUser(usersData, userid)
+        const userProfile = getUserProfile(usersProfile,user.uid)
+
+        console.log(getAge(userProfile.birthdate))
+        if(getAge(userProfile.birthdate) > 9) {
+            return false
+        }
+
+        return true
+    }
+
     return {
-      isEmpty: isEmpty
+      isEmpty: isEmpty,
+      isRegisteredUser: isRegisteredUser,
+      isUserUnderAge: isUserUnderAge
     };
   })();
   
